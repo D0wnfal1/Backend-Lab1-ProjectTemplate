a7ee9bee165fb906959e9d8dc08338e1b2a33d20
D0wnfal1 <deadofgreaf@gmail.com>
2024-09-17 03:00:46 +0300

    Add HealthCheck endpoint with current date and status

---
+++ Backend-Lab1-ProjectTemplate/Controllers/HealthCheckController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend_Lab1_ProjectTemplate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet("/healthcheck")]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                status = "Healthy",
+                timestamp = DateTime.UtcNow
+            });
+        }
+    }
+}
